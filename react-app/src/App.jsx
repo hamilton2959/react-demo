60970c8043577c2b7ee4a148140b7c7496dcccec
hamilton2959 <skytech2959@gmail.com>
2026-09-10 22:30:56 +0300

    initial commit

---
+++ react-app/src/App.jsx
@@ -0,0 +1,10 @@
+
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+
+function App() {
+  //return <Header />
+  return <Footer />
+}
+
+export default App
---
+++ react-app/src/Header.jsx
@@ -0,0 +1,22 @@
+
+function Header() {
+    return (
+        <div>
+            <header>
+                <h1>My Website</h1>
+
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <hr></hr>
+            </header>
+        </div>
+    );
+}
+
+export default Header
